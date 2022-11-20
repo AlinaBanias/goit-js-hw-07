71ec48d9a4adf5540669e369a95c11e09ba2658f
AlinaBanias <isabella08092001@gmail.com>
2022-11-20 20:17:35 +0200

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,73 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const imageMarkup = createImageMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
+ 
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+   
+   const isGalleryItem = event.target.classList.contains("gallery__image");
+   if(!isGalleryItem) {
+       return;
+   }
+   console.log(isGalleryItem);
+
+   const instance = basicLightbox.create(
+    ` <div class="modal"> 
+      <img src="${event.target.dataset.source}" alt="Big Pictures" width= "800" height= "600"/>
+      </div> `,
+    {
+        onShow: (instance) => {
+        galleryContainer.addEventListener("keydown", onEscapeButton);
+    },
+       onClose: (instance) => {
+        galleryContainer.removeEventListener("keydown", onEscapeButton);
+},
+}
+);
+instance.show();
+
+function onEscapeButton(evt) {
+    if(evt.code === "Escape") {
+        instance.close();
+    }
+    }
+
+   onGalleryContainerClick(isGalleryItem);
+}
+
+
+
+
+
+
+
+function createImageMarkup(images) {
+    return images.map(({ preview , original , description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+        </div>
+        `;
+    }).
+    join("");
+  
+}
+
+
+
+
+
